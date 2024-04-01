841df2f03d84d000cce42c398c31868116765cca
Ruslan-Dobrianskyi <130309553+Ruslan-Dobrianskyi@users.noreply.github.com>
2024-04-01 19:39:27 +0300

    update input

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,10 +1,13 @@
+import s from "./SearchBox.module.css";
+
 const SearchBox = ({ searchStr, onChangeSearch }) => {
   return (
     <div>
       <h2>Find contacts by name</h2>
       <input
+        className={s.input}
         type="text"
-        placeholder="Search user"
+        placeholder="Search user..."
         value={searchStr}
         onChange={(e) => onChangeSearch(e.target.value)}
       />
